cdab4d4f13dbf4ba49b473cef7b25be5969634d1
Mylodie <mylodie7@gmail.com>
2022-06-02 16:59:21 +0800

    Init Raft.rpc

--- Raft/raft/config.go
+++ Raft/raft/config.go
@@ -8,8 +8,8 @@ package raft
 // test with the original before submitting.
 //
 
-import "6.824/labgob"
-import "6.824/labrpc"
+import "Raft/labgob"
+import "Raft/labrpc"
 import "bytes"
 import "log"
 import "sync"
